8cfc78926e5d953365bf31395841a2f9fe71613a
Yihuan Yan <yjy5196@psu.edu>
2024-03-01 22:03:30 -0500

    dddd

--- channel.c
+++ channel.c
@@ -278,6 +278,44 @@ enum channel_status channel_destroy(channel_t* channel)
     return SUCCESS;
 }
 
+void unlock_channel_list(select_t* channel_list, size_t channel_count){
+    for (int i = 0; i < channel_count; ++i) {
+        int duplicate = 0;
+        for (int j = 0; j < i; ++j) {
+            if(channel_list[j].channel == channel_list[i].channel){
+                duplicate = 1;
+            }
+        }
+
+        if(duplicate == 0){
+            pthread_mutex_unlock(&(channel_list[i].channel->mutex));
+        }
+    }
+}
+
+void try_lock_channels(select_t* channel_list, size_t channel_count){
+    while(1){
+        int succeed = 1;
+        for (int i = 0; i < channel_count; ++i) {
+            int duplicate = 0;
+            for (int j = 0; j < i; ++j) {
+                if(channel_list[j].channel == channel_list[i].channel){
+                    duplicate = 1;
+                }
+            }
+            if(duplicate == 0){
+                if(pthread_mutex_trylock(&(channel_list[i].channel->mutex)) != 0){
+                    succeed = 0;
+                    unlock_channel_list(channel_list, (size_t)i);
+                    break;
+                }
+            }
+        }
+        if(succeed == 1)
+            return;
+    }
+}
+
 // Takes an array of channels (channel_list) of type select_t and the array length (channel_count) as inputs
 // This API iterates over the provided list and finds the set of possible channels which can be used to invoke the required operation (send or receive) specified in select_t
 // If multiple options are available, it selects the first option and performs its corresponding action
@@ -287,6 +325,88 @@ enum channel_status channel_destroy(channel_t* channel)
 // Additionally, selected_index is set to the index of the channel that generated the error
 enum channel_status channel_select(select_t* channel_list, size_t channel_count, size_t* selected_index)
 {
-    return SUCCESS;
-}
+    /* IMPLEMENT THIS */
+    pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+    pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
+    while(1){
+        try_lock_channels(channel_list, channel_count);
+        for (int i = 0; i < channel_count; ++i) {
+            //pthread_mutex_lock(&(channel_list[i].channel->mutex));
+            list_t *l;
+            if(channel_list[i].dir == SEND)
+                l = channel_list[i].channel->channel_selects_send;
+            else
+                l = channel_list[i].channel->channel_selects_recvive;
+            list_node_t* n = list_find(l, channel_list);
+            if(n != NULL){
+                list_remove(l, n);
+            }
+            //pthread_mutex_unlock(&(channel_list[i].channel->mutex));
+        }
+
+        for (int i = 0; i < channel_count; ++i) {
+            if(channel_list[i].dir == SEND){
+                enum channel_status s = channel_non_blocking_send_critical(channel_list[i].channel, channel_list[i].data);
+                //printf("thread %lx: send to channel %d, %d\n", pthread_self(), i, s);
+                //fflush(stdout);
+                //enum channel_status s = channel_non_blocking_send(channel_list[i].channel, channel_list[i].data);
+                if(s != CHANNEL_FULL){
+                    *selected_index = (size_t)i;
+                    //pthread_mutex_unlock(&mutex);
+                    unlock_channel_list(channel_list, channel_count);
+                    pthread_mutex_destroy(&(mutex));
+                    pthread_cond_destroy(&(cond));
+                    return s;
+                }
+            }else{
+                enum channel_status s = channel_non_blocking_receive_critical(channel_list[i].channel, &(channel_list[i].data));
+                //printf("thread %lx: recv from channel %d, %d\n", pthread_self(), i, s);
+                //fflush(stdout);
+                //enum channel_status s = channel_non_blocking_receive(channel_list[i].channel, &(channel_list[i].data));
+                if(s != CHANNEL_EMPTY){
+                    *selected_index = (size_t)i;
+                    //pthread_mutex_unlock(&mutex);
+                    unlock_channel_list(channel_list, channel_count);
+                    pthread_mutex_destroy(&(mutex));
+                    pthread_cond_destroy(&(cond));
+                    return s;
+                }
+                //pthread_cond_signal(&(channel_list[i].channel->generator_cond));
+            }
+        }
+
+        //printf("Thread %lx wait in select: ", pthread_self());
+        pthread_mutex_lock(&mutex);
+        channel_list->mutex = &mutex;
+        channel_list->cond = &cond;
+        for (int i = 0; i < channel_count; ++i) {
+            //pthread_mutex_lock(&(channel_list[i].channel->mutex));
+            if(channel_list[i].dir == RECV){
+                list_insert(channel_list[i].channel->channel_selects_recvive, channel_list);
+                //printf(" RECV on channel %lx", (unsigned long)(channel_list[i].channel));
+            }
+            else{
+                list_insert(channel_list[i].channel->channel_selects_send, channel_list);
+                //printf(" SEND on channel %lx", (unsigned long)(channel_list[i].channel));
+            }
+
+
+            /*if(channel_list[i].dir == RECV && channel_list[i].channel->status != CHANNEL_EMPTY){
+                printf(" recv when have data %d", channel_list[i].channel->status);
+                fflush(stdout);
+            }else if(channel_list[i].dir == SEND && channel_list[i].channel->status != CHANNEL_FULL){
+                printf(" send when have no %d", channel_list[i].channel->status);
+                fflush(stdout);
+            }*/
+            //pthread_mutex_unlock(&(channel_list[i].channel->mutex));
+        }
+        //printf("\n");
+        //fflush(stdout);
+
+        unlock_channel_list(channel_list, channel_count);
+        pthread_cond_wait(&cond, &mutex);
+        pthread_mutex_unlock(&mutex);
+        //printf("Thread %lx awake in select\n", pthread_self());
+    }
+}
